f19233c418a3ca94b449ebb8e8cac4b503cb697a
arun-kumar34 <arun.kumar@bootlabstech.com>
2022-03-10 00:34:03 +0530

    feat(crypto_key): support multiple keys and distinct members for each key

--- main.tf
+++ main.tf
@@ -5,18 +5,24 @@ resource "google_kms_key_ring" "gcp_kms_keyring" {
 }
 
 resource "google_kms_crypto_key" "gcp_kms_crypto_key" {
-  count           = var.prevent_destroy ? length(var.kms_key_name) : 0
-  name            = var.kms_key_name[count.index]
+  for_each = { for kms_crypto_key in var.kms_crypto_keys : kms_crypto_key.name => kms_crypto_key }
+  name            = each.key
   key_ring        = google_kms_key_ring.gcp_kms_keyring.id
-  labels          = var.labels
-  rotation_period = var.rotation_period
-  purpose         = var.purpose
-  version_template {
-    algorithm        = var.algorithm
-    protection_level = var.protection_level
+  labels          = each.value.labels
+  rotation_period = each.value.rotation_period
+  
+  purpose         = each.purpose
+
+  dynamic "version_template" {
+    for_each         = each.value.set_version_template == true ? [1] : []
+    content{
+      algorithm        = each.value.algorithm
+      protection_level = each.value.protection_level
+    }
   }
+
   lifecycle {
-    prevent_destroy = true
+    prevent_destroy = each.value.prevent_destroy
   }
 }
 
@@ -28,8 +34,8 @@ resource "google_kms_key_ring_iam_binding" "kms_keyring_iam_binding" {
 }
 
 resource "google_kms_crypto_key_iam_binding" "kms_key_iam_decrypter" {
-  for_each      = setunion(var.kms_key_members)
-  crypto_key_id = google_kms_crypto_key.gcp_kms_crypto_key[*].id
-  role          = "roles/cloudkms.cryptoKeyDecrypter"
-  members       = each.value
+  for_each = { for kms_crypto_key in var.kms_crypto_keys : kms_crypto_key.name => kms_crypto_key }
+  crypto_key_id = google_kms_crypto_key.gcp_kms_crypto_key[each.key].id
+  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+  members       = each.value.members
 }
