0d195c5df89138bcd92fadb74dab2d14ecb9ce6a
arun-kumar34 <arun.kumar@bootlabstech.com>
2022-03-10 12:04:04 +0530

    feat(name): modified name

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_kms_key_ring" "gcp_kms_keyring" {
-  name     = "${var.kms_keyring_name}-keyring"
+  name     = "${var.kms_keyring_name}"
   location = var.location_id
   project  = var.project_id
 }
